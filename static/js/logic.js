4f44e608c8e80ab8f0712ae5749bd8ab7922c73f
Jennis Vicente <jennis.m.vicente@gmail.com>
2019-09-30 14:03:09 -0700

    Update logic.js

--- static/js/logic.js
+++ static/js/logic.js
@@ -6,7 +6,7 @@
 var API_KEY = "pk.eyJ1IjoiamVubmlzbWFyaWUiLCJhIjoiY2swbjVsN3diMWU5eTNjbzN4NTl3OWwyaiJ9.SIMls8rCpizz7NIHt06dtA";
 var lightmap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/light-v9/tiles/256/{z}/{x}/{y}?access_token={accessToken}", {
   attribution: "Map data &copy; <a href=\"http://openstreetmap.org\">OpenStreetMap</a> contributors, <a href=\"http://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"http://mapbox.com\">Mapbox</a>",
-  maxZoom: 18,
+  maxZoom: 75,
   id: "mapbox.light",
   accessToken: API_KEY
 });
